7e7920b7be4c6783ca884d4b9369a2e89d144397
Evyatar Haim <evyataros1@gmail.com>
2021-01-12 21:53:59 +0200

    clean up process

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvrnfEFpasPvlqmnAuWY3EnJAetTYEqKY",
+    authDomain: "snapchat-evyatar-haim.firebaseapp.com",
+    projectId: "snapchat-evyatar-haim",
+    storageBucket: "snapchat-evyatar-haim.appspot.com",
+    messagingSenderId: "968421433688",
+    appId: "1:968421433688:web:2c1621a44c7b773afc052d",
+    measurementId: "G-TX77JRPDFB"
+  };
